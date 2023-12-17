37517ba5e6de9801dc6e3dcf74f18080de907f45
dwikigunawan123 <154036316+dwikigunawan123@users.noreply.github.com>
2023-12-17 12:31:46 +0700

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Toko Buku Online</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body class="bg-secondary">
+    <div class="container p-0 mb-4 mt-4 rounded-3 shadow bg-white">
+
+      <!-- MENU -->
+      <nav class="d-md-flex p-4">
+        <div><h1>BOOK STORE</h1></div>
+        <div class="ms-auto my-auto">
+          <ul class="list-inline m-0">
+            <li class="list-inline-item mx-md-3"><a href="#produk" class="text-decoration-none text-dark fw-bold">PRODUK KAMI</a></li>
+            <li class="list-inline-item mx-md-3"><a href="#tentang" class="text-decoration-none text-dark fw-bold">TENTANG KAMI</a></li>
+            <li class="list-inline-item mx-md-3"><a href="#alamat" class="text-decoration-none text-dark fw-bold">ALAMAT KAMI</a></li>
+            <li class="list-inline-item mx-md-3"><a href="login.php" class="text-decoration-none text-dark fw-bold">FORM PEMBELIAN</a></li>
+          </ul>
+        </div>
+      </nav>
+    
+      <!-- BANNER -->
+      <div class="px-4 mb-4">
+        <img src="images/6.png" class="w-100 rounded-4" />
+      </div>
+
+      <!-- MENU BUKU -->
+      <h3 class="text-center" id="produk">Buku Best Seller</h3>
+      <div class="text-center w-50 mx-auto fw-light">Daftar buku terbaik dan terlaris di BOOK STORE</div>
+
+      <div class="row row-cols-md-3 row-cols-2 gx-5 p-5">
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/03.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Belajar Microsoft Office (Word, Excel, Powerpoint) 2019</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Microsoft Office (Word, Excel, Powerpoint) 2019
+              <br>Penulis : Johnie Rogers Swanda Pasaribu, S.Kom., M.Kom.
+              <br>Kategori Buku: Sains dan Teknologi
+              <br>ISBN : 978-623-02-6106-0
+              <br>Ukuran: 17.5×25 cm
+              <br>Halaman: xviii, 182 hlm
+              <br>Tahun Terbit : 2022 <br>Deskripsi buku:
+              <br>Microsoft Office yang antara terdiri dari aplikasi Word, Excel dan Powerpoint merupakan aplikasi pengolah kata, aplikasi pengolah angka dan presentasi yang sangat dibutuhkan manusia dan yang paling banyak digunakan pengguna komputer saat ini, Aplikasi Ms. Office (Word, Excel dan Powerpoint) saat ini dapat mempermudah pekerjaan kantoran ataupun segala pekerjaan yang erat kaitannya dengan kegiatan administrasi lainnya dan aplikasi ini sangat dibutuhkan berbagai kalangan, antara lain siswa SMP, SMA/SMK, mahasiswa, karyawan, pegawai maupun berbagai bidang profesi lainnya.</i></p></div>
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 108.000.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/09.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Sistem Pendukung Keputusan Teori dan Implementasi</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Sistem Pendukung Keputusan Teori Dan Implementasi
+              <br>Tanggal Rilis: 13 September 2019
+              <br>Penulis: Lita Asyriati Latif, Mohamad Jamil dan Said HI Abbas
+              <br>Halaman: 107 Halaman
+              <br>Penerbit:Deepublish <br>Deskripsi Buku:
+              <br>Buku ini menjelaskan teori dan implementasi berbagai metode dasar dalam sistem pendukung keputusan disertai studi kasus dalam penentuan pemenang tender proyek pemerintah. Yang dapat digunakan dalam menunjang perkuliahan khususnya matakuliah sistem pendukung keputusan.</i></p></div>
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 61.000.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/04.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Psikologi Komunikasi: Teori dan Praktik</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Psikologi Komunikasi: Teori dan Praktik 
+              <br>Penulis : Markus Utomo Sukendar
+              <br>viii, 76 hlm.; Uk:14x20 cm
+              <br>ISBN 978-602-453-397-7
+              <br>Cetakan Oktober 2017. <br>Deskripsi Buku:
+              <br>Berbicara tentang Psikologi Komunikasi, tidak dapat dilepaskan dengan komunikasi itu sendiri. Dalam sejarah perkembangan komunikasi juga banyak melibatkan banyak tokoh Psikologi di dalamnya. Setidaknya ada 3 (tiga) orang pakar Psikologi yang juga diakui sebagai Bapak Komunikasi, yaitu Carl I. Hovland, Paul Lazarfeld dan Kurt Lewin. Meskipun demikian, komunikasi tidak menjadi bagian dalam subdisiplin keilmuan psikologi.</i></p></div>
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 58.500.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/05.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Iklim Organisasi Era Digital (Konseptual dan Operasionalisasi)</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Iklim Organisasi Era Digital (Konseptual & Operasionalisasi)
+              <br>Pengarang : Dr. Fakhry Zamzam, M.M., M.H., CIQaR., CIQnR., Dr. Tien Yustini, S.E., M.Si., CIRR., CLMA., Havis Aravik, M.Si. M.M.
+              <br>Penerbit : Deepublish, 2021
+              <br>ISBN : 6230226073, 9786230226076
+              <br>Tebal : 180 halaman <br>Deskripsi Buku:
+              <br>Iklim organisasi adalah saudara kembarnya dari budaya organisasi, namun dalam perkembangannya, tidak banyak orang yang tertarik untuk mengkaji tentang teori iklim organisasi. Sehingga kalah populer dibandingkan dengan teori budaya organisasi. Kendati terminologi iklim organisasi sudah dikenalkan oleh Kurt Lewin sejak tahun 1939 yang mengadopsi dari istilah iklim psikologi, kendati demikian teori ini sesungguhnya banyak digunakan dalam mengukur iklim organisasi pada dunia pendidikan.</i></p></div>
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 131.000.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/06.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Metodologi Penelitian Akuntansi (Teori Dan Praktek)</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Metodologi Penelitian Akuntansi (Teori Dan Praktek)
+              <br>Penulis : Bambang Sudaryana
+              <br>Ukuran :viii, 83 hlm, Uk: 17.5x25 cm
+              <br>ISBN :-781-0
+              <br>Cetakan Pertama :Juni 2019 <br>Deskripsi Buku:
+              <br>Banyak pendapat tentang arti penelitian, maka perlu dilakukan pembahasan secara jelas apa sebenarnya penelitian tersebut, sehingga akan mempermudah berbagai pihak mengenal dan memahami tentang penelitian. Penelitian adalah suatu cara guna menyelesaikan permasalahan dengan menekan segala keterbatasan dan ketidaktahuan manusia. Atau dengan pengertian lain bahwa penelitian adalah merupakan suatu pemikiran yang rasional untuk melakukan aktivitas-aktivitas penelitian, mengumpulkan fakta-fakta yang ada dan kemudian memprosesnya sehingga peneliti dapat mengombinasikan dengan menggunakan metode ilmiah, sehingga permasalahan yang ada dan pertanyaan yang tidak terjawab dapat dipecahkan atau diselesaikan. Penelitian sebagai suatu proses ilmiah tentunya menggunakan karakteristik keilmuan yaitu secara rasional, sistematis dan berdasarkan fakta empiris.</i></p></div>
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 80.500.00</span>
+            </div>
+          </div>
+        </div>
+        <div class="col mb-5">
+          <div class="card shadow">
+            <img src="images/08.jpg" class="card-img-top" />
+            <div class="card-body">
+              <p class="card-text">Buku Ajar Pendidikan Kewarganegaraan di indonesia</p>
+            </div>
+            <div class="d-none deskripsi"><p><i>Buku Pendidikan Kewarganegaraan Di Indonesia
+              <br>Penulis : Ipik Permana
+              <br>Ukuran : x, 128 hlm, Uk: 14x20 cm
+              <br>ISBN : 978-602-475-918-6
+              <br>Cetakan Pertama: November 2018
+              <br>Penerbit : Deepublsh <br>Deskripsi Buku:
+              <br>Topik yang dibahas dalam buku ini meliputi bela Negara, masyarakat bangsa, warganegara, ideologi, konstitusi, demokrasi, hak asasi manusia, civil society masyarakat umum, wawasan nusantara dan ketahanan nasional.</i></p></div>   
+            <div class="card-footer d-md-flex">
+              <a class="btn btn-sm btn-primary d-block btnDetail">detail</a>
+              <span class="ms-auto text-danger fw-bold d-block text-center harga">Rp. 66.500.00</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+       <!-- TENTANG KAMI -->
+       <div id="tentang" class="px-4 py-4 bg-secondary text-center">
+        <div class="mx-auto w-75">
+          <h3 class="text-white">TENTANG KAMI</h3>
+          <p class="text-center text-white"><img src="images/about-us.png" align="left" style="width: 100px; height: auto" class="me-3 mb-3">Selamat datang di toko buku kita yang penuh dengan kata-kata dan petualangan tak terbatas! Di sini, setiap halaman adalah peluang untuk menemukan pengetahuan baru, mengeksplorasi cerita yang menarik, dan merasakan keajaiban kata-kata. Bersama, kita adalah penjelajah kata yang tak kenal batas, siap menjelajahi dunia literasi dan membawa pulang harta karun kata-kata. Selamat menikmati petualangan baca yang tak terlupakan di toko buku ini!</p>
+        </div>
+      </div>
+
+        <!-- ALAMAT KAMI -->
+      <section id="alamat" class="px-4 py-4 bg-body-secondary text-center">
+        <div class="mx-auto w-75">
+        </div>
+        <h3 class="text-black">ALAMAT KAMI</h3>
+        <p>Jika datang langsung ke toko kami sudah tertera map di bawah
+        </p>
+  
+        <div class="row">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31758.7325024718!2d105.57299735539362!3d-5.735944531749332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e4111d14a0acafd%3A0x56be9a7c1bc8436!2sKalianda%2C%20Kec.%20Kalianda%2C%20Kabupaten%20Lampung%20Selatan%2C%20Lampung!5e0!3m2!1sid!2sid!4v1696362752250!5m2!1sid!2sid"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            class="map"
+          ></iframe>
+
+            <!-- copyright -->
+      <div class="text-center p-4 border-top">&copy; 2023 BOOK STORE - Toko Buku Online</div>
+    </div>
+
+    <!-- modal -->
+<button type="button" class="btn btn-primary d-none btnModal" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5 modalTitle" id="exampleModalLabel"></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body row">
+      <div class="modalImage col-md-6 col-12"></div>
+      <div class="col-md-6 col-12">
+        <div class="modalDeskripsi"></div>
+        <div class="d-md-flex">
+          <span class="ms-auto text-danger fw-bold d-block text-center modalHarga"></span>
+        </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="javascript/main.js"></script>
+  </body>
+</html>
